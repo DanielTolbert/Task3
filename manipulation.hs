56bbf797c9fe1bf5decb7af0fde8621594164772
Daniel Tolbert <danielchristopher19@gmail.com>
2019-05-27 16:34:45 -0400

    Made the three methods

---
+++ manipulation.hs
@@ -0,0 +1,7 @@
+methOne x ss = if even x then ss else reverse ss
+
+helperTwo xs = if even $ length xs then xs else reverse xs
+methTwo xss = map helperTwo $ xss
+
+boustrophedon xss = [ methOne x y | (x, y) <- zip xss [0..] ]
+
